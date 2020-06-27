995d6f7a82e51e1babb541dd3a21393785a11872
Vlad Shcherban <vlad@coronalabs.com>
2020-06-27 10:04:46 +0300

    random changes to trigger update

--- plugins/2014.2430/iphone-sim/extract.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-lipo libfacebook.a -remove armv7 -output libfacebook.a
--- plugins/2014.2430/iphone/extract.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-lipo libfacebook.a -remove x86_64 -output libfacebook.a
-lipo libfacebook.a -remove i386 -output libfacebook.a
